b6864fef51cd1253808c838cbb5b64d8d4f44310
Colin Biafore <cbia4@users.noreply.github.com>
2017-06-09 21:23:10 -0700

    Singleton returns InputStream object from S3 to be deserialized by AvroReader

--- src/main/java/S3Connector.java
+++ src/main/java/S3Connector.java
@@ -1,41 +1,51 @@
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.S3Object;
+
+import java.io.*;
 
 /**
  * Created by colinbiafore on 6/8/17.
  */
 public class S3Connector {
 
+    private static String bucket;
+    private static String key;
+
     // Singleton
     private S3Connector(){}
 
-    public static void run() {
+    private static void readConfig() throws IOException {
+        InputStream fis = new FileInputStream("src/main/resources/config");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        bucket = br.readLine();
+        key = br.readLine();
+    }
 
+    public static InputStream getObjectContent() {
+
+        try {
+            readConfig();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
         // returns default credentials
         AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
+        AmazonS3 s3client = AmazonS3Client.builder()
+                .withRegion("us-east-1")
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
 
-        // TODO: Replace AmazonS3Client with non-deprecated equivalent
-        AmazonS3 s3client = new AmazonS3Client(credentials);
-
-        // TODO: Read String literals from configuration
         String bucketName = "castle-kafka-export";
-        ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName).withMaxKeys(2);
-        ListObjectsV2Result result;
-        do {
-            result = s3client.listObjectsV2(req);
+        String keyName = "events/event_v1/2017-05/2017-05-31-event_v1-000726-1497023490196.part.avro";
 
-            for(S3ObjectSummary objectSummary : result.getObjectSummaries()) {
-                System.out.println(" - " + objectSummary.getKey() + " " + "(size = " + objectSummary.getSize() + ")");
-            }
-            System.out.println("Next Continuation Token : " + result.getNextContinuationToken());
-            req.setContinuationToken(result.getNextContinuationToken());
-        } while(result.isTruncated());
+        S3Object object = s3client.getObject(bucket,key);
+
+        return object.getObjectContent();
+
     }
 }
