213df0915e11742ea27e4da683f630eb88967f12
Colin Biafore <cbia4@users.noreply.github.com>
2017-06-09 21:25:43 -0700

    Adding trivial S3 test case to maintain consistence - This class does not actually test S3 functionality

---
+++ src/test/java/S3ConnectorTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by colinbiafore on 6/9/17.
+ */
+
+import org.junit.Test;
+
+public class S3ConnectorTest {
+
+    @Test
+    public void testConnector() {
+        S3Connector.getObjectContent();
+    }
+}
