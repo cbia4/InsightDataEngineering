07202ac475f2e2f232a7c62c3354399714a10659
Colin Biafore <cbia4@users.noreply.github.com>
2017-06-16 21:59:32 -0700

    Renamed AvroReader to AvroUtility, added functionality to encode and decode a GenericRecord <-> byte[] with Twitter bijection library

---
+++ src/main/java/AvroUtility.java
@@ -0,0 +1,57 @@
+import com.twitter.bijection.Injection;
+import com.twitter.bijection.avro.GenericAvroCodecs;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileStream;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.EOFException;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by colinbiafore on 6/8/17.
+ */
+public class AvroUtility {
+
+    private Schema schema;
+    private Injection<GenericRecord,byte[]> recordInjection;
+
+    public AvroUtility(String path) throws IOException {
+        //this.schemaFile = new File(path);
+        this.schema = new Schema.Parser().parse(new File(path));
+        this.recordInjection = GenericAvroCodecs.toBinary(this.schema);
+    }
+
+    public Schema getSchema() { return schema; }
+
+    public byte[] encode(GenericRecord record) {
+        return recordInjection.apply(record);
+    }
+
+    public GenericRecord decode(byte[] bytes) {
+        return recordInjection.invert(bytes).get();
+    }
+
+    public List<GenericRecord> getRecords(InputStream inputStream) throws IOException  {
+
+        List<GenericRecord> eventList = new ArrayList<>();
+        DatumReader<GenericRecord> datumReader = new GenericDatumReader<>(this.schema);
+        DataFileStream<GenericRecord> dataFileStream = new DataFileStream<>(inputStream, datumReader);
+        Iterator<GenericRecord> recordIterator = dataFileStream.iterator();
+
+        while(recordIterator.hasNext()) {
+            eventList.add(recordIterator.next());
+        }
+
+        return eventList;
+
+    }
+
+}
