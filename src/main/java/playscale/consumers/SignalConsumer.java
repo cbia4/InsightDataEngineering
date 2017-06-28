cccbf5f08a0dd87fc841217907c62fb8be61986d
Colin Biafore <cbia4@users.noreply.github.com>
2017-06-28 00:10:22 -0700

    Updated SignalConsumer to be runnable - implemented run to indefinitely consume from kafka brokers until WakeupException is thrown by the new SignalConsumerGroup - created SignalConsumerGroup to manage a group of consumers and shut them down properly when the application is terminated

--- src/main/java/playscale/consumers/SignalConsumer.java
+++ src/main/java/playscale/consumers/SignalConsumer.java
@@ -1,54 +1,72 @@
 package playscale.consumers;
 
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
-
-public class SignalConsumer {
+/**
+ * This is a runnable signal consumer class
+ * Instances of this class are created by the SignalConsumerGroup
+ * The consumer will run indefinitely until it receives a WakeupException
+ * when the SignalConsumerGroup calls shutdown() for each instance of the consumer
+ */
+public class SignalConsumer implements Runnable {
 
     private final Logger logger = Logger.getLogger(SignalConsumer.class);
-    private Consumer<Long,byte[]> consumer;
-    private Properties properties;
+    private final KafkaConsumer<Long,byte[]> consumer;
+    private final List<String> topics;
 
-    public SignalConsumer(Properties properties) throws IOException {
+    public SignalConsumer(Properties configProperties) {
 
-        String subscriberList = properties.getProperty("consumer.topics");
-        String servers = properties.getProperty("consumer.bootstrap.servers");
-        String timeout = properties.getProperty("consumer.session.timeout.ms");
-        this.properties = properties;
+        this.topics = Arrays.asList(configProperties.getProperty("consumer.topic"));
+        String servers = configProperties.getProperty("consumer.bootstrap.servers","localhost:9092");
+        String groupId = configProperties.getProperty("consumer.group.id");
 
         Properties props = new Properties();
-        props.put("bootstrap.servers",servers);
-        props.put("group.id", "signals");
+        props.put("bootstrap.servers", servers);
+        props.put("group.id",groupId);
+        props.put("key.deserializer", LongDeserializer.class.getName());
+        props.put("value.deserializer", ByteArrayDeserializer.class.getName());
+
+        // commit offset every second to reduce duplicate records being processed (in the event of a consumer crash)
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
-        props.put("session.timeout.ms",timeout);
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.LongDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+
+        // this sets the amount of time the consumer has to respond to the kafka coordinator
+        // before the consumer is considered "dead" and kafka re-balances the group
+        props.put("session.timeout.ms","60000");
         this.consumer = new KafkaConsumer<>(props);
-        this.consumer.subscribe(Arrays.asList(subscriberList));
-
-
     }
 
+    // get records from the specified topic until a WakeupException is thrown from the SignalConsumerGroup
+    @Override
     public void run() {
+        try {
+            consumer.subscribe(topics);
 
-        long pollRate = Long.parseLong(properties.getProperty("consumer.pollrate","100"));
-
-        while (true) {
-            ConsumerRecords<Long, byte[]> records = consumer.poll(pollRate);
-            for (ConsumerRecord<Long, byte[]> record : records) {
-                String jsonString = new String(record.value());
-                logger.info("Signal received: " + jsonString);
+            while(true) {
+                ConsumerRecords<Long,byte[]> records = consumer.poll(Long.MAX_VALUE);
+                for(ConsumerRecord<Long,byte[]> record : records) {
+                    String jsonString = new String(record.value());
+                    logger.info("Signal received: " + jsonString);
+                }
             }
+        } catch (WakeupException e) {
+            // ignore for shutdown
+        } finally {
+            consumer.close();
         }
     }
 
+    public void shutdown() {
+        consumer.wakeup();
+    }
 }
---
+++ src/main/java/playscale/consumers/SignalConsumerGroup.java
@@ -0,0 +1,65 @@
+package playscale.consumers;
+
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Consumer Group Class
+ * The run() is called from the PlayScale driver when "consumer" is selected
+ * This class will create the number of consumers specified in the config.properties file
+ * under the specified consumer group id - consumers will consume from the topics given
+ */
+public class SignalConsumerGroup {
+
+    private final Logger logger = Logger.getLogger(SignalConsumerGroup.class);
+    private final int numConsumers;
+    private final String groupId;
+    private final Properties configProperties;
+
+    public SignalConsumerGroup(Properties configProperties) {
+        this.configProperties = configProperties;
+        this.numConsumers = Integer.parseInt(configProperties.getProperty("consumer.group.threads","1"));
+        this.groupId = configProperties.getProperty("consumer.group.id");
+    }
+
+    // Create a thread executor service, a list of consumers, and execute them one by one
+    public void run() {
+
+        ExecutorService executor = Executors.newFixedThreadPool(numConsumers);
+
+        List<SignalConsumer> consumers = new ArrayList<>();
+        for(int i = 0; i < numConsumers; i++) {
+            SignalConsumer consumer = new SignalConsumer(configProperties);
+            consumers.add(consumer);
+            executor.submit(consumer);
+        }
+
+        // Safely shut down the consumer group when SIGTERM signal received
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+
+            @Override
+            public void run() {
+                for(SignalConsumer consumer : consumers) {
+                    consumer.shutdown();
+                }
+                executor.shutdown();
+                try {
+                    long timeout = 5000;
+                    executor.awaitTermination(timeout, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    logger.error("InterruptedException thrown while shutting down Consumer Group with groupId " + groupId);
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+
+}
